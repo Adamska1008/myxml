c8b5d672b0f771fa8cf430ceafef73f0bc6bc772
adamska <2639980868@qq.com>
2024-08-28 13:19:56 +0800

    chore(paser.hpp): add method comment

--- src/parser.hpp
+++ src/parser.hpp
@@ -33,18 +33,17 @@ namespace myxml
         std::string buffer;
         std::size_t offset;
 
-        /**
-         * TODO:
-         * Define Exceptions , so for all parsing method,
-         * return std::nullopt means `not this one`,
-         * and throw exception means `parsing error`
-         */
-
         void skipWhiteSpaces();
         std::optional<char> peekChar();
         std::optional<std::string> peekNextNChars(int);
         std::optional<char> nextChar();
         std::optional<std::string> nextNChars(int);
+
+        /**
+         * For all parsing method,
+         * return std::nullopt means `not this one` and will not consume buffer.
+         * Throw exception means `parsing error` and should stop immediately
+         */
 
         /**
          * Parse an identity.
@@ -59,31 +58,39 @@ namespace myxml
          */
         std::string parseStringLiteral();
         /**
-         * @returns std::nullopt if find no attribute 
+         * @returns std::nullopt if find no attribute
          * @throws `UnexpectedEndOfInput`
          * @throws `SyntaxError` if the following chars do not confront to `key="value"` format
          */
         std::optional<std::pair<std::string, std::string>> parseAttribute();
         std::shared_ptr<Text> parseText();
-        // return the entire element
-        // will consume buffer if failed
+        /**
+         * @throws `UnexpectedEndOfInput`
+         * @throws `SyntaxError`
+         * @throws `SemanticError`
+         */
         std::shared_ptr<Element> parseElementWithHeader(ElementTag header);
         /**
          * @returns std::nullopt if not starts with `<?xml`
+         * @throws `UnexpectedEndOfInput`
          * @throws `SyntaxError`
          * @throws `SemanticError`
          */
         std::optional<Declaration> parseDeclaration();
 
     public:
-        // return and consume current element
-        // will consume buffer if failed
         std::optional<std::shared_ptr<Element>> ParseElement();
         /**
          * @returns std::nullopt if no heading `<`
-         * @throws `SyntaxError` if the heading character is `<` and the trailing characters are in incorrect format 
+         * @throws `SyntaxError` if the heading character is `<` and the trailing characters are in incorrect format
+         * @throws `UnexpectedEndOfInput` if missing name
          */
         std::optional<ElementTag> ParseTag();
+        /**
+         * @throws `UnexpectedEndOfInput`
+         * @throws `SyntaxError`
+         * @throws `SemanticError`
+         */
         Document ParseDocument();
         Parser() = delete;
         explicit Parser(std::string_view);
